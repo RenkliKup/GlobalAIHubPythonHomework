6e2214fbe3f9a1d372b8d105cde04164151aa6c1
Baris Karakulak <baris.1907.krklk@gmail.com>
2020-12-26 15:20:01 +0300

    HW3 eklendi

---
+++ HW3/HW3.py
@@ -0,0 +1,48 @@
+import random as rnd
+'''
+Kurallar:
+5 hak veriliyor buldun buldun :D
+'''
+name=input("please enter yourself name:")
+words=["a b i l i t y"] 
+letter=""
+ayni=[]
+game=rnd.choice(words)
+game=game.split(" ")
+count=0
+deneme=5
+print(f"Hello {name}")
+while True:
+
+    letter=input("lütfen bir harf girin:")
+    if(letter in ayni):
+        print("bu harfi daha önce girdiniz")
+        pass
+    else:
+        for a in range(0,len(game)):
+            if letter==game[a]:
+                ayni.append(letter)
+                cnt=len(game)-1
+                print(f"güzel {letter} harfini buldun. şimdi {cnt} harf kaldı")
+                count+=1
+                while True:
+                    try:
+                        game.remove(letter)
+                    except ValueError:
+                        break
+                break
+        if(deneme <1):
+            print("oyun bitti")
+            break
+
+        if(count==0):
+            deneme-=1
+            print(f"deneme {deneme} kaldı ")
+        if(count>0 and deneme>0):
+            count=0
+        if(len(game)==0):
+            print("tebrikler oyunu kazandın")
+            break
+
+
+    

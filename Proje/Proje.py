5dc1cad78f8724c87826cc9eb71d491859b618ea
Baris Karakulak <baris.1907.krklk@gmail.com>
2020-12-27 22:24:11 +0300

    Proje Güncellendi 2

--- Proje/Proje.py
+++ Proje/Proje.py
@@ -54,56 +54,76 @@ case={
 print("Öğrenci Kaydınızı Yapın Lütfen")
 name=input("Lütfen Adınızı Giriniz:")
 surname=input("Lütfen soyadınızı giriniz:")
-obje=s_s_s(name,surname)
 print("Öğrenci Kaydınız Başarılı.")
-kontrol=True
-while kontrol:
-    print(f"Merhaba {obje.name} {obje.surname}")
-    for x in case.values():
-        if x in "Yanlış Tuş Girişi":
-            pass
-        else:
-            print(x)
-    switch=int(input("please enter someone:"))
-   
-    if switch==1:
-        if(obje.count>0):
-            yeniad=input(f"{obje.count} defa adınızı ve soyadınızı değiştirebilirsiniz Yeni Ad Giriniz:")
-            yeniSoyad=input(f"{obje.count} defa adınızı ve soyadınızı değiştirebilirsiniz Yeni Soyad Giriniz:")
-            obje.adDegistir(yeniad,yeniSoyad)
-            print(f"Adınız ve soyadınız {obje.name} {obje.surname} olarak değiştirildi")
-        else:
-            print("Hakkınız bitti")
-    elif switch==2:
-        if(obje.dersSayisi>5):
-            print("Daha Fazla Ders Giremezsiniz")
-        else:
-            dersAdi=input("Ders Adını Giriniz:")
-            vize=int(input("Vize Notunu Girniz:"))
-            final=int(input("Final Notunu Girniz:"))
-            proje=int(input("Proje Notunu Giriniz:"))
-            obje.dersTanimla(dersAdi,vize,final,proje)
-            print("Ders Kaydınız Başarılı")
-    elif switch==3:
-        if(obje.dersSayisi>2):
-              obje.dersNotHesapla()
-        else:
-            print(f"Ders sayınız en az 3 olmalı. sizin ders sayınız: {obje.dersSayisi}")
-    elif switch==4:
-        if(obje.dersSayisi>0):
-            obje.dersKayiKeys()
-        else:
-            print("Dersiniz Yok Lütfen ders ekleyin")
-    else:
-        case["Default"]
-    kontrol=input("Devam Etmek İstiyor musunz E/H")
-    if kontrol=="e" or kontrol=="E":
-        kontrol=True
-    else:
-        kontrol=False
-        print("Tekrar Bekleriz")
+name_surname_control=True
+
+obje=s_s_s(name,surname)
+print("Öğrenci Girişini Yapın Lütfen")
+for control in range(0,3):
     
-            
-            
+    name=input("Lütfen Adınızı Giriniz:")
+    surname=input("Lütfen soyadınızı giriniz:")
+    
+    print(control)
+    if((obje.name==name and obje.surname==surname)):
+        print("Giriş Yapıldı")
+        name_surname_control=True
+        break
+    else:
+        name_surname_control=False
+if(not name_surname_control):
+    print("Yanlış Ad veya Soyad girdiniz programa giriş yapılmadı")
+    pass
+else:
+    
+    kontrol=True
+    while kontrol:
+        print(f"Merhaba {obje.name} {obje.surname}")
+        for x in case.values():
+            if x in "Yanlış Tuş Girişi":
+                pass
+            else:
+                print(x)
+        switch=int(input("please enter someone:"))
+    
+        if switch==1:
+            if(obje.count>0):
+                yeniad=input(f"{obje.count} defa adınızı ve soyadınızı değiştirebilirsiniz Yeni Ad Giriniz:")
+                yeniSoyad=input(f"{obje.count} defa adınızı ve soyadınızı değiştirebilirsiniz Yeni Soyad Giriniz:")
+                obje.adDegistir(yeniad,yeniSoyad)
+                print(f"Adınız ve soyadınız {obje.name} {obje.surname} olarak değiştirildi")
+            else:
+                print("Hakkınız bitti")
+        elif switch==2:
+            if(obje.dersSayisi>5):
+                print("Daha Fazla Ders Giremezsiniz")
+            else:
+                dersAdi=input("Ders Adını Giriniz:")
+                vize=int(input("Vize Notunu Girniz:"))
+                final=int(input("Final Notunu Girniz:"))
+                proje=int(input("Proje Notunu Giriniz:"))
+                obje.dersTanimla(dersAdi,vize,final,proje)
+                print("Ders Kaydınız Başarılı")
+        elif switch==3:
+            if(obje.dersSayisi>2):
+                obje.dersNotHesapla()
+            else:
+                print(f"Ders sayınız en az 3 olmalı. sizin ders sayınız: {obje.dersSayisi}")
+        elif switch==4:
+            if(obje.dersSayisi>0):
+                obje.dersKayiKeys()
+            else:
+                print("Dersiniz Yok Lütfen ders ekleyin")
+        else:
+            case["Default"]
+        kontrol=input("Devam Etmek İstiyor musunz E/H")
+        if kontrol=="e" or kontrol=="E":
+            kontrol=True
+        else:
+            kontrol=False
+            print("Tekrar Bekleriz")
         
-   
+                
+                
+            
+    
